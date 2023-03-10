59afa1a9fca0ac6992ef77c29950b5f1f2de7744
Alexsander Alves <lex.naldi@outlook.com>
2023-03-10 10:41:25 -0300

    Primeiro Commit

---
+++ src/main/java/com/example/projetoQuintoAndar/ProjetoQuintoAndarApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetoQuintoAndar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoQuintoAndarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoQuintoAndarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projetoQuintoAndar/ProjetoQuintoAndarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetoQuintoAndar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoQuintoAndarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
